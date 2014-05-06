b9103f6c428c3e58be2efff10276708d2b48fc96
J. Ryan Stinnett <jryans@gmail.com>
2014-05-05 22:09:49 -0500

    Enable dump

--- b2g/simulator/custom-prefs.js
+++ b2g/simulator/custom-prefs.js
@@ -1,3 +1,4 @@
 user_pref("devtools.debugger.prompt-connection", false);
 user_pref("devtools.debugger.forbid-certified-apps", false);
 user_pref("javascript.options.discardSystemSource", false);
+user_pref("browser.dom.window.dump.enabled", true);
