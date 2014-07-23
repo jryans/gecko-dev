f9d69d3baca992ec338341b3dc95ebe6d136d476
J. Ryan Stinnett <jryans@gmail.com>
2014-07-23 15:10:28 -0500

    Sign the cert

--- toolkit/devtools/security/DevToolsCertService.cpp
+++ toolkit/devtools/security/DevToolsCertService.cpp
@@ -43,13 +43,13 @@ DevToolsCertService::GetOrCreateCert(nsIX509Cert** aCert)
     return NS_ERROR_FAILURE;
   }
 
-  nsCOMPtr<nsIX509Cert> cert;
+  nsCOMPtr<nsIX509Cert> certFromDB;
   nsresult rv;
   rv = certDB->FindCertByNickname(nullptr, certName,
-                                  getter_AddRefs(cert));
+                                  getter_AddRefs(certFromDB));
   if (NS_SUCCEEDED(rv)) {
     // TODO: Verify cert is good
-    mCert = cert;
+    mCert = certFromDB;
     *aCert = mCert;
     NS_IF_ADDREF(*aCert);
     return NS_OK;
@@ -89,6 +89,77 @@ DevToolsCertService::GetOrCreateCert(nsIX509Cert** aCert)
     return NS_ERROR_FAILURE;
   }
   publicKey = tempPublicKey;
+
+  // Create subject public key info and cert request
+  ScopedCERTSubjectPublicKeyInfo spki(
+    SECKEY_CreateSubjectPublicKeyInfo(publicKey));
+  if (!spki) {
+    return NS_ERROR_FAILURE;
+  }
+  ScopedCERTCertificateRequest certRequest(
+    CERT_CreateCertificateRequest(subjectName, spki, nullptr));
+  if (!certRequest) {
+    return NS_ERROR_FAILURE;
+  }
+
+  // Valid from one day before to 2 years after
+  static const PRTime oneDay = PRTime(PR_USEC_PER_SEC)
+                             * PRTime(60)  // sec
+                             * PRTime(60)  // min
+                             * PRTime(24); // hours
+
+  PRTime now = PR_Now();
+  PRTime notBefore = now - oneDay;
+  PRTime notAfter = now + (PRTime(365) * PRTime(2) * oneDay);
+  ScopedCERTValidity validity(CERT_CreateValidity(notBefore, notAfter));
+  if (!validity) {
+    return NS_ERROR_FAILURE;
+  }
+
+  // Generate random serial
+  unsigned long serial;
+  // Note: This serial in principle could collide, but it's unlikely
+  SECStatus srv =
+    PK11_GenerateRandomOnSlot(slot,
+                              reinterpret_cast<unsigned char *>(&serial),
+                              sizeof(serial));
+  if (srv != SECSuccess) {
+    return NS_ERROR_FAILURE;
+  }
+
+  // Create the cert from these pieces
+  ScopedCERTCertificate cert(
+    CERT_CreateCertificate(serial, subjectName, validity, certRequest));
+  if (!cert) {
+    return NS_ERROR_FAILURE;
+  }
+
+  // Update the cert version to X509v3
+  *(cert->version.data) = SEC_CERTIFICATE_VERSION_3;
+  cert->version.len = 1;
+
+  // Set cert signature algorithm
+  PLArenaPool* arena = cert->arena;
+  srv = SECOID_SetAlgorithmID(arena, &cert->signature,
+                              SEC_OID_PKCS1_SHA1_WITH_RSA_ENCRYPTION, 0);
+  if (srv != SECSuccess) {
+    return NS_ERROR_FAILURE;
+  }
+
+  // Encode and self-sign the cert
+  ScopedSECItem certDER(
+    SEC_ASN1EncodeItem(arena, nullptr, cert,
+                       SEC_ASN1_GET(CERT_CertificateTemplate)));
+  if (!certDER) {
+    return NS_ERROR_FAILURE;
+  }
+  srv = SEC_DerSignData(arena, &cert->derCert, certDER->data, certDER->len,
+                        privateKey, SEC_OID_PKCS1_SHA1_WITH_RSA_ENCRYPTION);
+  if (srv != SECSuccess) {
+    return NS_ERROR_FAILURE;
+  }
+
+
 
   return NS_ERROR_NOT_IMPLEMENTED;
 }
