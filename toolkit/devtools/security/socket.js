c9215cb58f0ff481a739bc15d76bf9a9b8de6b69
J. Ryan Stinnett <jryans@gmail.com>
2014-12-11 17:46:10 -0600

    sample client id prompt

--- toolkit/devtools/security/socket.js
+++ toolkit/devtools/security/socket.js
@@ -6,7 +6,7 @@
 
 "use strict";
 
-let { Ci, Cc, CC, Cr } = require("chrome");
+let { Ci, Cc, CC, Cr, Cu } = require("chrome");
 
 // Ensure PSM is initialized to support TLS sockets
 Cc["@mozilla.org/psm;1"].getService(Ci.nsISupports);
@@ -49,8 +49,6 @@ DevToolsUtils.defineLazyGetter(this, "nssErrorsService", () => {
 
 DevToolsUtils.defineLazyModuleGetter(this, "Task",
   "resource://gre/modules/Task.jsm");
-
-const DBG_STRINGS_URI = "chrome://global/locale/devtools/debugger.properties";
 
 let DebuggerSocket = {};
 
@@ -166,6 +164,11 @@ let AuthenticationResult = DebuggerSocket.AuthenticationResult = createEnum({
  *        Whether the server requires encryption.  Defaults to false.
  * @param authentication Authentication (optional)
  *        Authentication mode expected by the server.  Defaults to PROMPT.
+ * @param cert object (optional)
+ *        The server's cert details.  Used with OOB_CERT authentication.
+ * @param prompt object (optional)
+ *        Override default prompts to customize UX.
+ *        TODO: Too many objects, etc.
  * @return promise
  *         Resolved to a DebuggerTransport instance.
  */
@@ -181,17 +184,58 @@ DebuggerSocket.connect = Task.async(function*(settings) {
 
   // We must be using OOB_CERT authentication
   let deferred = promise.defer();
+  let oob;
+  let showPrompt = settings.prompt.sendOOB ||
+                   prompt.Client.defaultSendOOB;
+  let activePrompt;
   transport.hooks = {
-    onPacket(packet) {
+    onPacket: Task.async(function*(packet) {
+      // Close any prompts the client may have been showing from previous
+      // authentication steps
+      if (activePrompt && activePrompt.close) {
+        activePrompt.close();
+        activePrompt = null;
+      }
       let { authResult } = packet;
+      switch (authResult) {
+        case AuthenticationResult.PENDING:
+          // OOB_CERT step B.8
+          // Client creates hash(ClientCert) + K(random 128-bit number)
+          oob = yield createOOB();
+          let details = Object.assign({}, settings);
+          details.authResult = authResult;
+          details.oob = oob;
+          activePrompt = showPrompt(details);
+          break;
+        default:
+          transport.close();
+          deferred.reject(new Error("Invalid auth result: " + authResult));
+          return;
+      }
+    }),
+    onClosed(reason) {
+      // Transport died before auth completed
+      deferred.reject(reason);
     }
   };
   transport.ready();
   return deferred.promise;
 });
 
-function defaultPendingNotification() {
+let createOOB = Task.async(function*() {
+  let clientCert = yield cert.local.getOrCreate();
+  return {
+    sha256: clientCert.sha256Fingerprint,
+    k: createRandom()
+  };
+});
 
+function createRandom() {
+  const length = 16; // 16 bytes / 128 bits
+  let rng = Cc["@mozilla.org/security/random-generator;1"]
+            .createInstance(Ci.nsIRandomGenerator);
+  let bytes = rng.generateRandomBytes(length);
+  return [for (byte of bytes) byte.toString(16)].join("");
 }
 
 /**
