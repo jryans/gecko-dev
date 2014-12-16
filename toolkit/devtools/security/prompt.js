6560715d2b8f9c553f536c0cb29d7b524dfc7230
J. Ryan Stinnett <jryans@gmail.com>
2014-12-15 18:30:14 -0600

    sample receive oob

--- toolkit/devtools/security/prompt.js
+++ toolkit/devtools/security/prompt.js
@@ -146,3 +146,20 @@ Server.defaultAllowConnection = ({ client, server }) => {
   }
   return DebuggerSocket.AuthenticationResult.DENY;
 };
+
+Server.defaultReceiveOOB = () => {
+  const key = "serverReceiveOOB";
+  let title = bundle.GetStringFromName(key + "Title");
+  let msg = bundle.GetStringFromName(key + "Body");
+  let input = { value: null };
+  let prompt = Services.prompt;
+  let result = prompt.prompt(null, title, msg, input, null, { value: false });
+  if (!result) {
+    return promise.reject();
+  }
+  // Re-create original object from token
+  let sha256 = input.value.substring(0, 64);
+  sha256 = sha256.replace(/\w{2}/g, "$&:").slice(0, -1).toUpperCase();
+  let k = input.value.substring(64);
+  return promise.resolve({ sha256, k });
+};
