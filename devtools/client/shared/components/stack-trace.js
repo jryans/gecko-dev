778d15580399b8e84279bd7de53a68da73738faa
J. Ryan Stinnett <jryans@gmail.com>
2017-02-03 16:54:14 -0600

    Bug 1336621 - Add keys in Frame and StackTrace components. r=Honza
    MozReview-Commit-ID: 8GUYouvklxA

--- devtools/client/shared/components/stack-trace.js
+++ devtools/client/shared/components/stack-trace.js
@@ -45,15 +45,17 @@ const StackTrace = createClass({
     } = this.props;
 
     let frames = [];
-    stacktrace.forEach(s => {
+    stacktrace.forEach((s, i) => {
       if (s.asyncCause) {
         frames.push("\t", AsyncFrame({
+          key: i,
           asyncCause: s.asyncCause
         }), "\n");
       }
 
       let source = s.filename.split(" -> ").pop();
       frames.push("\t", Frame({
+        key: i,
         frame: {
           functionDisplayName: s.functionName,
           source,
