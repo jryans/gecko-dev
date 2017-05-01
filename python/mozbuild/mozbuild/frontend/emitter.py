f46983e1383d29d2c6ebae849013133d572e8e60
J. Ryan Stinnett <jryans@gmail.com>
2017-05-01 13:19:57 -0500

    codegen-units=1 for debug symbols
    MozReview-Commit-ID: 4tQvSYz7wT8

--- python/mozbuild/mozbuild/frontend/emitter.py
+++ python/mozbuild/mozbuild/frontend/emitter.py
@@ -475,7 +475,7 @@ class TreeMetadataEmitter(LoggingMixin):
                         'rpath': False,
                         'lto': False,
                         'debug-assertions': True,
-                        'codegen-units': 4,
+                        'codegen-units': 1,
                         'panic': 'abort',
                     }
                 else:
