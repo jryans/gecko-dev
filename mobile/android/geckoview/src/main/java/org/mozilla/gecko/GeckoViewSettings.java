Floorp-Projects/Floorp

Bug 1369815 - Add display mode to GeckoViewSettings r=esawin

mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoViewSettings.java
@@ -22,6 +22,24 @@ public final class GeckoViewSettings {
         }
     }
 
+    public enum DisplayMode {
+        // This needs to match nsIDocShell.idl
+        BROWSER(0),
+        MINIMAL_UI(1),
+        STANDALONE(2),
+        FULLSCREEN(3);
+
+        private final int mMode;
+
+        DisplayMode(int mode) {
+            mMode = mode;
+        }
+
+        public int value() {
+            return mMode;
+        }
+    }
+
     /*
      * Key to enabled and disable tracking protection.
      */
@@ -32,6 +50,13 @@ public final class GeckoViewSettings {
      */
     public static final Key<Boolean> USE_PRIVATE_MODE =
         new Key<Boolean>("usePrivateMode");
+
+    /*
+     * Key to specify which display-mode we should use
+     */
+    public static final Key<Boolean> USE_DISPLAY_MODE =
+        new Key<Boolean>("useDisplayMode");
+
     /*
      * Key to enabled and disable multiprocess browsing (e10s).
      * Note: can only be set during GeckoView initialization, changes during an
@@ -75,6 +100,23 @@ public final class GeckoViewSettings {
     public boolean getBoolean(final Key<Boolean> key) {
         synchronized (mBundle) {
             return mBundle.getBoolean(key.text);
+        }
+    }
+
+    public void setInt(final Key<Boolean> key, int value) {
+        synchronized (mBundle) {
+            final Object old = mBundle.get(key.text);
+            if (old != null && old.equals(value)) {
+                return;
+            }
+            mBundle.putInt(key.text, value);
+        }
+        dispatchUpdate();
+    }
+
+    public int getInt(final Key<Boolean> key) {
+        synchronized (mBundle) {
+            return mBundle.getInt(key.text);
         }
     }
 

jryans/gecko-dev

mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoViewSettings.java
@@ -22,6 +22,24 @@ public final class GeckoViewSettings {
         }
     }
 
+    public enum DisplayMode {
+        // This needs to match nsIDocShell.idl
+        BROWSER(0),
+        MINIMAL_UI(1),
+        STANDALONE(2),
+        FULLSCREEN(3);
+
+        private final int mMode;
+
+        DisplayMode(int mode) {
+            mMode = mode;
+        }
+
+        public int value() {
+            return mMode;
+        }
+    }
+
     /*
      * Key to enabled and disable tracking protection.
      */
@@ -32,6 +50,7 @@ public final class GeckoViewSettings {
      */
     public static final Key<Boolean> USE_PRIVATE_MODE =
         new Key<Boolean>("usePrivateMode");
+
     /*
      * Key to enabled and disable multiprocess browsing (e10s).
      * Note: can only be set during GeckoView initialization, changes during an
@@ -39,6 +58,13 @@ public final class GeckoViewSettings {
      */
     public static final Key<Boolean> USE_MULTIPROCESS =
         new Key<Boolean>("useMultiprocess");
+
+    /*
+     * Key to specify which display-mode we should use
+     */
+    public static final Key<Boolean> DISPLAY_MODE =
+        new Key<Boolean>("displayMode");
+
 
     private final EventDispatcher mEventDispatcher;
     private final GeckoBundle mBundle;
@@ -54,6 +80,7 @@ public final class GeckoViewSettings {
         setBoolean(USE_TRACKING_PROTECTION, false);
         setBoolean(USE_PRIVATE_MODE, false);
         setBoolean(USE_MULTIPROCESS, true);
+        setInt(DISPLAY_MODE, DisplayMode.BROWSER.value());
     }
 
     /* package */ GeckoViewSettings(GeckoViewSettings settings, EventDispatcher eventDispatcher) {
@@ -75,6 +102,23 @@ public final class GeckoViewSettings {
     public boolean getBoolean(final Key<Boolean> key) {
         synchronized (mBundle) {
             return mBundle.getBoolean(key.text);
+        }
+    }
+
+    public void setInt(final Key<Boolean> key, int value) {
+        synchronized (mBundle) {
+            final Object old = mBundle.get(key.text);
+            if (old != null && old.equals(value)) {
+                return;
+            }
+            mBundle.putInt(key.text, value);
+        }
+        dispatchUpdate();
+    }
+
+    public int getInt(final Key<Boolean> key) {
+        synchronized (mBundle) {
+            return mBundle.getInt(key.text);
         }
     }
 