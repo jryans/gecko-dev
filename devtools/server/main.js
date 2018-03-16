e86d3537ec5632aeba6a5d73f52406ffcaadd4bb
J. Ryan Stinnett <jryans@gmail.com>
2018-03-15 19:10:29 -0500

    Resources: Wire up actor and front
    MozReview-Commit-ID: 3hKj1p0KaRS

--- devtools/server/main.js
+++ devtools/server/main.js
@@ -537,6 +537,11 @@ var DebuggerServer = {
       constructor: "AccessibilityActor",
       type: { target: true }
     });
+    this.registerModule("devtools/server/actors/resources", {
+      prefix: "resources",
+      constructor: "ResourcesActor",
+      type: { target: true }
+    });
   },
 
   /**
